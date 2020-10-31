1cd2d93ab10be258ddf7a89e509d6abd35c100d6
Jalen <jalen.mendonca@gmail.com>
2020-10-30 21:50:27 -0400

    Updated Folders

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,54 @@
-module.exports = {
+//module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
-};
+  //"URI": "mongodb://localhost/books229"
+//};
+
+
+//mongo "mongodb+srv://comp229midterm.gitxn.mongodb.net/COMP229Midterm" --username dbUser
+
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+
+const url = "mongodb+srv://comp229midterm.gitxn.mongodb.net/COMP229Midterm";
+
+const client = new MongoClient(url);
+ 
+ // The database to use
+ const dbName = "test";
+                      
+ async function run() {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db(dbName);
+
+         // Use the collection "people"
+         const col = db.collection("people");
+
+         // Construct a document                                                                                                                                                              
+         let personDocument = {
+             "name": { "first": "Alan", "last": "Turing" },
+             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+             "views": 1250000
+         }
+
+         // Insert a single document, wait for promise so we can read it back
+         const p = await col.insertOne(personDocument);
+         // Find one document
+         const myDoc = await col.findOne();
+         // Print to the console
+         console.log(myDoc);
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
